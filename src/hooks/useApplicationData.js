1987b9263191a56849df6f0daf2efbed87b0688c
Justin Ly <jly3@ualberta.ca>
2021-03-22 18:56:54 -0600

    refactor reducer, added reducer test

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,73 +1,14 @@
 import { useReducer, useEffect } from "react";
 import axios from "axios";
 
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW,
+  UPDATE_SPOTS,
+} from "reducers/application";
+
 export default function useApplicationData() {
-
-  const SET_DAY = "SET_DAY";
-  const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-  const SET_INTERVIEW = "SET_INTERVIEW";
-  const UPDATE_SPOTS = "UPDATE_SPOTS";
-
-  function reducer(state, action) {
-    
-    switch (action.type) {
-
-    case SET_DAY:
-      return {
-        ...state,
-        day: action.day
-      };
-
-    case SET_APPLICATION_DATA:
-      return {
-        ...state,
-        days: action.days,
-        appointments: action.appointments,
-        interviewers: action.interviewers
-      };
-
-    case SET_INTERVIEW: {
-      return {
-        ...state, appointments: action.appointments
-      };
-    }
-
-    case UPDATE_SPOTS: {
-      return {
-        ...state, days: spotsRemaining(state)
-      };
-    }
-
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-    }
-  }
-
-  function spotsRemaining(currentState) {
-    //create copies of the state, declare variables
-    let currentDay = {};
-    let daysCopy = [...currentState.days];
-    let appointmentsCopy = {...currentState.appointments};
-    let count;
-    //loop through dayItem, and update the spots for each day
-    for (const dayItem of daysCopy) {
-      //establish initial available spots from day object, and establish currentDay
-      count = dayItem.appointments.length;
-      currentDay = {...dayItem};
-      //check which appointments currently do not have an appointment
-      for (const ID of currentDay.appointments) {
-        if (appointmentsCopy[ID].interview !== null) {
-          count--;
-        }
-      }
-      //update currentDay with spots, update the days array at the index with the new spot count
-      currentDay.spots = count;
-      daysCopy[currentDay.id - 1] = currentDay;
-    }
-    return daysCopy;
-  }
 
   const [state,dispatch] = useReducer(reducer, {
     day: "Monday",
--- src/reducers/application.js
+++ src/reducers/application.js
@@ -1,8 +1,32 @@
 
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
-const UPDATE_SPOTS = "UPDATE_SPOTS";
+export const SET_DAY = "SET_DAY";
+export const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+export const SET_INTERVIEW = "SET_INTERVIEW";
+export const UPDATE_SPOTS = "UPDATE_SPOTS";
+
+function spotsRemaining(currentState) {
+  //create copies of the state, declare variables
+  let currentDay = {};
+  let daysCopy = [...currentState.days];
+  let appointmentsCopy = {...currentState.appointments};
+  let count;
+  //loop through dayItem, and update the spots for each day
+  for (const dayItem of daysCopy) {
+    //establish initial available spots from day object, and establish currentDay
+    count = dayItem.appointments.length;
+    currentDay = {...dayItem};
+    //check which appointments currently do not have an appointment
+    for (const ID of currentDay.appointments) {
+      if (appointmentsCopy[ID].interview !== null) {
+        count--;
+      }
+    }
+    //update currentDay with spots, update the days array at the index with the new spot count
+    currentDay.spots = count;
+    daysCopy[currentDay.id - 1] = currentDay;
+  }
+  return daysCopy;
+}
 
 function reducer(state, action) {
     
@@ -40,3 +64,5 @@ function reducer(state, action) {
     );
   }
 }
+
+export default reducer;
